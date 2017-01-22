9e5c736077aac061caf171cf7f74da44170820b9
Jim Moore <jmoore@reico.com>
2017-01-21 19:06:49 -0500

    Adding blocks

--- README.md
+++ README.md
@@ -2,3 +2,26 @@
 Align a class in Visual RPG
 
 If you're using the Visual RPG by ASNA then this may be handy trying to format your text.
+
+General conventions:
+We indent with 2 spaces for every black we create. i.e.: 
+BegClass
+  xxx
+  xxx
+  BegSr
+    if this
+      xxx
+    else
+      xxx
+    endif
+
+We code DclDiskFile in blocks like this:
+  Dcldiskfile name(pcm) +
+    type(*input) +
+    org(*indexed) +
+    file("*libl/file") +
+    db(reiconet) +
+    chkfmtid(*no) +
+    impopen(*yes)  
+
+We expect an indentation for KList's and PList's
--- visual_rpg_align.rb
+++ visual_rpg_align.rb
@@ -4,23 +4,28 @@ class VisualRpgAlign
   def self.process(file_name)
     indent_type = nil
     indent_count = 0
+    block_parm = false
     File.open("#{file_name}.out", 'w') do |f|
       File.open(file_name) do |file|
         file.each do |line|
           line.gsub!(/\t/, '  ')  # Tabs are converted to 2 spaces. Yeah, they are embedded everywhere.
-          else_tag = line.strip.downcase == 'else' ? 1 : 0
+          recompense = line.strip.downcase == 'else' ? -1 : 0
+          recompense = 1 if (line.downcase.include?("dclkfld") || line.downcase.include?("dclparm"))
+          recompense = 1  if block_parm
           indent_count-=1  if CLOSING.map { |t| line.downcase.include?(t) }.any?  unless line.strip.slice(0,2) == "//"
           if indent_count > 0
-            f.puts "#{ '  ' * (indent_count - else_tag) }#{ line.strip }"
+            f.puts "#{ '  ' * (indent_count + recompense) }#{ line.strip }"
           else
-            f.puts line
+            f.puts line.strip
           end
           indent_count+=1  if TYPES.map { |t| line.downcase.include?(t) && !(line.downcase.include?("endif")) }.any?  unless line.strip.slice(0,2) == "//"
+          block_parm = true  if line.downcase.include?("dcldiskfile")
+          block_parm = false  if (line.strip == "" || line.strip.nil?)
         end
       end
     end
   end
 end
 
-VisualRpgAlign.process("Default.aspx.vr")
+VisualRpgAlign.process("report.aspx.vr")
 puts "Done"
